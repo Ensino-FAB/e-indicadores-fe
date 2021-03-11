690a6946f55642c0ce3da75e2c4d4f20803b7c71
andreibarroso <andrei.mesquita@globomail.com>
2021-03-11 14:19:35 -0300

    fix :hammer: Corrige keylocak redirect

--- src/environments/environment.homolog.ts
+++ src/environments/environment.homolog.ts
@@ -5,5 +5,5 @@ export const environment = {
   KEYCLOAK_REALM: 'FAB',
   KEYCLOAK_CLIENT_ID: 'ensino-client',
   KEYCLOAK_REDIRECT_URI:
-    'https://sigpes.servicos.homolog.ccarj.intraer/esistens-organizacional/assets/silent-check-sso.html',
+    'https://sigpes.servicos.homolog.ccarj.intraer/esistens-indicadores/assets/silent-check-sso.html',
 };
