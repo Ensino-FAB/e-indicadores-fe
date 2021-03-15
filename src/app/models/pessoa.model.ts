6d1fb8f5d89fb225c4ccfe42be59811fdd2ffe6c
lucaslbatista <lucaslbatista@gmail.com>
2021-03-15 11:33:53 -0300

    feat (diplomados): :art: adiciona sigla, posto e especialidade para a lista de diplomados

--- src/app/models/pessoa.model.ts
+++ src/app/models/pessoa.model.ts
@@ -1,10 +1,12 @@
-import {Organizacao} from './organizacao.model';
-import {BaseModel} from './base.model';
+import { Organizacao } from './organizacao.model';
+import { BaseModel } from './base.model';
 
 export interface Pessoa extends BaseModel {
   nome: string;
   nrCpf: string;
   nrOrdem?: string;
   siglaPosto?: string;
+  siglaQuadro?: string;
+  siglaEspecialidade?: string;
   organizacao?: Organizacao;
 }
